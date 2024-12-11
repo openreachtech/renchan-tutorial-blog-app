2d742efff53456d15e53a727e03a3ac345c0ae1e
JYama321 <yamajiiii54@gmail.com>
2024-12-11 11:43:01 +0700

    add migration files

---
+++ sequelize/migrations/20241211035850-create_table-admin_usernames.cjs
@@ -0,0 +1,42 @@
+'use strict'
+
+const MigrationAttributeFactory = require('@openreachtech/renchan-sequelize/lib/tools/MigrationAttributeFactory.cjs')
+
+const TABLE_NAME = 'admin_usernames'
+const COLUMN_NAME = {
+  USERNAME: 'username',
+  SAVED_AT: 'saved_at',
+}
+
+module.exports = {
+  async up (
+    queryInterface,
+    Sequelize
+  ) {
+    const factory = MigrationAttributeFactory.create(Sequelize)
+
+    return queryInterface.createTable(TABLE_NAME, {
+      ...factory.ID_BIGINT,
+
+      username: {
+        type: Sequelize.STRING(191),
+        field: COLUMN_NAME.USERNAME,
+        allowNull: false,
+      },
+      savedAt: {
+        type: Sequelize.DATE(3),
+        field: COLUMN_NAME.SAVED_AT,
+        allowNull: false,
+      },
+
+      ...factory.TIMESTAMPS,
+    })
+  },
+
+  async down (
+    queryInterface,
+    Sequelize
+  ) {
+    return queryInterface.dropTable(TABLE_NAME)
+  },
+}
---
+++ sequelize/migrations/20241211040239-create_table-admin_secrets_bk.cjs
@@ -0,0 +1,59 @@
+'use strict'
+
+const MigrationAttributeFactory = require('@openreachtech/renchan-sequelize/lib/tools/MigrationAttributeFactory.cjs')
+
+const TABLE_NAME = 'admin_secrets_bk'
+const COLUMN_NAME = {
+  ADMIN_ID: 'admin_id',
+  EMAIL: 'email',
+  SAVED_AT: 'saved_at',
+}
+
+module.exports = {
+  async up (
+    queryInterface,
+    Sequelize
+  ) {
+    const factory = MigrationAttributeFactory.create(Sequelize)
+
+    return queryInterface.createTable(TABLE_NAME, {
+      ...factory.ID_BIGINT,
+
+      AdminId: {
+        type: Sequelize.BIGINT,
+        field: COLUMN_NAME.ADMIN_ID,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING(191),
+        field: COLUMN_NAME.EMAIL,
+        allowNull: false,
+      },
+      savedAt: {
+        type: Sequelize.DATE(3),
+        field: COLUMN_NAME.SAVED_AT,
+        allowNull: false,
+      },
+
+      ...factory.TIMESTAMPS,
+    })
+      .then(() => queryInterface.addIndex(
+        TABLE_NAME,
+        [COLUMN_NAME.ADMIN_ID],
+        {
+          name: [
+            TABLE_NAME,
+            COLUMN_NAME.ADMIN_ID,
+            'index'
+          ].join('_'),
+        }
+      ))
+  },
+
+  async down (
+    queryInterface,
+    Sequelize
+  ) {
+    return queryInterface.dropTable(TABLE_NAME)
+  },
+}
